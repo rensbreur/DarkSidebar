4d1d26cc58406d1898f3837afa1df2af5a59de76
Rens Breur <rens.breur@live.com>
2019-02-06 17:11:51 +0100

    Initial commit

---
+++ DarkSidebar/DarkSidebar/DarkSidebar.h
@@ -0,0 +1,13 @@
+//
+//  DarkSidebar.h
+//  DarkSidebar
+//
+//  Created by Rens Breur on 06/02/2019.
+//  Copyright © 2019 Rens Breur. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DarkSidebar : NSObject
+
+@end
---
+++ DarkSidebar/DarkSidebar/DarkSidebar.m
@@ -0,0 +1,13 @@
+//
+//  DarkSidebar.m
+//  DarkSidebar
+//
+//  Created by Rens Breur on 06/02/2019.
+//  Copyright © 2019 Rens Breur. All rights reserved.
+//
+
+#import "DarkSidebar.h"
+
+@implementation DarkSidebar
+
+@end
